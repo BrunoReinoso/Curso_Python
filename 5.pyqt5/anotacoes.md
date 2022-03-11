f9cdf7ef070566d3f670c9635485837357973e3f
bruno <bruno.teixeira@dafiti.com.br>
2022-03-11 10:43:31 -0300

    aula 154 calculadora

---
+++ 5.pyqt5/anotacoes.md
@@ -0,0 +1,4 @@
+# Seção 6: PyQT5 - Interface Gráfica no Python
+
+O ``PyQT`` é um toolkit desenvolvido em C++ utilizado por vários programas para criação de aplicações GUI (Interface gráfica). Possui diversas outras funcionalidades, como acesso a base de dados, threads, comunicação de rede, etc.
+
---
+++ 5.pyqt5/aula_153.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QGridLayout
+
+class App(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+
+        self.btn = QPushButton('Texto do botão')
+        self.btn.setStyleSheet('font-size: 40px;')
+        self.grid.addWidget(self.btn, 0, 0, 1, 1)
+
+        self.btn.clicked.connect(self.click)
+        self.setCentralWidget(self.cw)
+
+    def click(self):
+        print('Criar algo aqui.')
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    app = App()
+    app.show()
+    qt.exec_()
